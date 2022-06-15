759b1cb7828c80feb67e231f64367ef206ce9ef3
erwan-png <erwan.v@free.fr>
2022-06-15 19:42:00 +0200

    add parser and manager for earning call ds + add stats file

--- src/data_parser/utils.py
+++ src/data_parser/utils.py
@@ -1,8 +1,13 @@
 from src.data_parser.essays_parser import DataUnification
+from src.data_parser.earning_calls_parser import DataParsing
 
 def ParseEssays():
 	DataUnification()
 
 def ParseWebDiscourse():
 	# to be implemented
+	pass
+
+def ParseEarningCalls():
+	DataParsing()
 	pass
